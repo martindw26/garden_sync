ab5298f7f3757ed36fe672a827cc5a50ab12c995
martin williams <86750566+martindw26@users.noreply.github.com>
2025-04-17 18:08:23 +0100

    updated page_php

--- page.php
+++ page.php
@@ -1,5 +1,15 @@
 <?php get_header(); ?>
-<main id="main">
-<?php while ( have_posts() ) : the_post(); the_content(); endwhile; ?>
+
+<main id="main" role="main">
+    <?php if ( have_posts() ) : ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+            <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+                <?php the_content(); ?>
+            </article>
+        <?php endwhile; ?>
+    <?php else : ?>
+        <p><?php esc_html_e( 'Sorry, no content found.', 'your-theme-textdomain' ); ?></p>
+    <?php endif; ?>
 </main>
+
 <?php get_footer(); ?>
